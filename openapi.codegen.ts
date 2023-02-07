ca35525adb75da3b8e4d43884470e6ab472bd443
songbinghui <soongbinghui@gmail.com>
2023-02-07 18:04:25 +0800

    feat: debugger

--- openapi.codegen.ts
+++ openapi.codegen.ts
@@ -1,20 +1,23 @@
-import { generateApi } from 'swagger-typescript-api';
-import path from 'path';
+import pkg from "swagger-typescript-api";
+const { generateApi, generateTemplates } = pkg;
+import path from "path";
 
-const URL = 'http://';
-const INPUT = './src/openapi/spec.json';
-const OUTPUT = './src/openapi/__service__';
-const TEMPLATES = './src/openapi/templates';
+const URL = "http://127.0.0.1:4523/export/openapi?projectId=2165099";
+const INPUT = "./src/openapi/spec.json";
+const OUTPUT = "./src/openapi/__service__";
+const TEMPLATES = "./src/openapi/templates";
+const SOURCEPLATES = "./src/openapi/source-templates";
 
 /* NOTE: all fields are optional expect one of `output`, `url`, `spec` */
 generateApi({
   output: path.resolve(process.cwd(), OUTPUT),
   templates: path.resolve(process.cwd(), TEMPLATES),
-  generateUnionEnums: true,
+  generateUnionEnums: false,
+  // 当 modular 为真时，注意类型名不能与文件同名
   modular: true,
   // url: URL,
   input: path.resolve(process.cwd(), INPUT),
-  httpClientType: 'axios',
+  httpClientType: "axios",
   defaultResponseAsSuccess: true,
   unwrapResponseData: true,
   generateClient: true,
@@ -30,26 +33,37 @@ generateApi({
   prettier: {
     printWidth: 120,
     tabWidth: 2,
-    trailingComma: 'all',
-    parser: 'typescript',
+    trailingComma: "all",
+    parser: "typescript",
   },
-  defaultResponseType: 'void',
+  defaultResponseType: "void",
   singleHttpClient: true,
   cleanOutput: true,
   enumNamesAsValues: false,
-  typePrefix: '',
-  typeSuffix: '',
-  enumKeyPrefix: 'Enum',
-  enumKeySuffix: '',
+  typePrefix: "",
+  typeSuffix: "",
+  enumKeyPrefix: "",
+  enumKeySuffix: "",
   addReadonly: false,
   extractingOptions: {
-    requestBodySuffix: ['Payload'],
-    requestParamsSuffix: ['Params'],
-    responseBodySuffix: ['Response'],
-    responseErrorSuffix: ['Error'],
+    requestBodySuffix: ["Payload"],
+    requestParamsSuffix: ["Params"],
+    responseBodySuffix: ["Response"],
+    responseErrorSuffix: ["Error"],
   },
   /** allow to generate extra files based with this extra templates, see more below */
   // extraTemplates: [],
-  fixInvalidTypeNamePrefix: 'Type',
-  fixInvalidEnumKeyPrefix: 'Value',
+  fixInvalidTypeNamePrefix: "Type",
+  fixInvalidEnumKeyPrefix: "Value",
+  hooks:{
+
+  }
+});
+
+generateTemplates({
+  cleanOutput: true,
+  output: path.resolve(process.cwd(), SOURCEPLATES),
+  httpClientType: "axios",
+  modular: true,
+  silent: false,
 });
--- src/openapi/__service__/Pet.ts
+++ src/openapi/__service__/Pet.ts
@@ -9,21 +9,8 @@
  * ---------------------------------------------------------------
  */
 
-import {
-  AddPetResponse,
-  FindPetsByStatusParams,
-  FindPetsByStatusResponse,
-  FindPetsByTagsParams,
-  FindPetsByTagsResponse,
-  GetPetByIdResponse,
-  Pet,
-  UpdatePetResponse,
-  UpdatePetWithFormParams,
-  UploadFileParams,
-  UploadFilePayload,
-  UploadFileResponse,
-} from "./data-contracts";
-import { ContentType, HttpClient, RequestParams } from "./http-client";
+import { FindPetsByStatusParams, FindPetsByStatusResponse } from "./data-contracts";
+import { HttpClient, RequestParams } from "./http-client";
 
 export class Pet<SecurityDataType = unknown> {
   http: HttpClient<SecurityDataType>;
@@ -32,48 +19,6 @@ export class Pet<SecurityDataType = unknown> {
     this.http = http;
   }
 
-  /**
-   * @description Update an existing pet by Id
-   *
-   * @tags pet
-   * @name UpdatePet
-   * @summary Update an existing pet
-   * @request PUT:/pet
-   * @secure
-   * @response `200` `UpdatePetResponse` Successful operation
-   * @response `400` `void` Invalid ID supplied
-   * @response `404` `void` Pet not found
-   * @response `405` `void` Validation exception
-   */
-  updatePet = (data: Pet, params: RequestParams = {}) =>
-    this.http.request<UpdatePetResponse, void>({
-      path: `/pet`,
-      method: "PUT",
-      body: data,
-      secure: true,
-      type: ContentType.Json,
-      ...params,
-    });
-  /**
-   * @description Add a new pet to the store
-   *
-   * @tags pet
-   * @name AddPet
-   * @summary Add a new pet to the store
-   * @request POST:/pet
-   * @secure
-   * @response `200` `AddPetResponse` Successful operation
-   * @response `405` `void` Invalid input
-   */
-  addPet = (data: Pet, params: RequestParams = {}) =>
-    this.http.request<AddPetResponse, void>({
-      path: `/pet`,
-      method: "POST",
-      body: data,
-      secure: true,
-      type: ContentType.Json,
-      ...params,
-    });
   /**
    * @description Multiple status values can be provided with comma separated strings
    *
@@ -90,98 +35,6 @@ export class Pet<SecurityDataType = unknown> {
       path: `/pet/findByStatus`,
       method: "GET",
       query: query,
-      secure: true,
-      ...params,
-    });
-  /**
-   * @description Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.
-   *
-   * @tags pet
-   * @name FindPetsByTags
-   * @summary Finds Pets by tags
-   * @request GET:/pet/findByTags
-   * @secure
-   * @response `200` `FindPetsByTagsResponse` successful operation
-   * @response `400` `void` Invalid tag value
-   */
-  findPetsByTags = (query: FindPetsByTagsParams, params: RequestParams = {}) =>
-    this.http.request<FindPetsByTagsResponse, void>({
-      path: `/pet/findByTags`,
-      method: "GET",
-      query: query,
-      secure: true,
-      ...params,
-    });
-  /**
-   * @description Returns a single pet
-   *
-   * @tags pet
-   * @name GetPetById
-   * @summary Find pet by ID
-   * @request GET:/pet/{petId}
-   * @secure
-   * @response `200` `GetPetByIdResponse` successful operation
-   * @response `400` `void` Invalid ID supplied
-   * @response `404` `void` Pet not found
-   */
-  getPetById = (petId: number, params: RequestParams = {}) =>
-    this.http.request<GetPetByIdResponse, void>({
-      path: `/pet/${petId}`,
-      method: "GET",
-      secure: true,
-      ...params,
-    });
-  /**
-   * No description
-   *
-   * @tags pet
-   * @name UpdatePetWithForm
-   * @summary Updates a pet in the store with form data
-   * @request POST:/pet/{petId}
-   * @secure
-   * @response `405` `void` Invalid input
-   */
-  updatePetWithForm = ({ petId, ...query }: UpdatePetWithFormParams, params: RequestParams = {}) =>
-    this.http.request<any, void>({
-      path: `/pet/${petId}`,
-      method: "POST",
-      query: query,
-      secure: true,
-      ...params,
-    });
-  /**
-   * @description delete a pet
-   *
-   * @tags pet
-   * @name DeletePet
-   * @summary Deletes a pet
-   * @request DELETE:/pet/{petId}
-   * @secure
-   * @response `400` `void` Invalid pet value
-   */
-  deletePet = (petId: number, params: RequestParams = {}) =>
-    this.http.request<any, void>({
-      path: `/pet/${petId}`,
-      method: "DELETE",
-      secure: true,
-      ...params,
-    });
-  /**
-   * No description
-   *
-   * @tags pet
-   * @name UploadFile
-   * @summary uploads an image
-   * @request POST:/pet/{petId}/uploadImage
-   * @secure
-   * @response `200` `UploadFileResponse` successful operation
-   */
-  uploadFile = ({ petId, ...query }: UploadFileParams, data: UploadFilePayload, params: RequestParams = {}) =>
-    this.http.request<UploadFileResponse, any>({
-      path: `/pet/${petId}/uploadImage`,
-      method: "POST",
-      query: query,
-      body: data,
       secure: true,
       ...params,
     });
